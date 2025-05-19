3f5e5383e929e01930c0c3221b9c467bf17ab2f4
newadmin <newadmin@DESKTOP-MKHM14B>
2025-05-20 08:28:49 +0900

    크론잡 UI/UX 버전 업데이트

--- src/main/java/com/src/auto/AutoMapper.java
+++ src/main/java/com/src/auto/AutoMapper.java
@@ -9,11 +9,9 @@ import com.src.vo.auto_send_log;
 
 @Mapper
 public interface AutoMapper {
-	int auto_total(auto_send paramauto_send);
 
-	List<auto_send> auto_check(auto_send paramauto_send);
-
-	void auto_update(auto_send paramauto_send);
-
-	void auto_log_insert(auto_send_log paramauto_send_log);
+	public int auto_total(auto_send paramauto_send);
+	public List<auto_send> auto_check(auto_send paramauto_send);
+	public void auto_update(auto_send paramauto_send);
+	public void auto_log_insert(auto_send_log paramauto_send_log);
 }
--- src/main/java/com/src/auto/AutoService.java
+++ src/main/java/com/src/auto/AutoService.java
@@ -2,15 +2,31 @@ package com.src.auto;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.src.vo.auto_send;
 import com.src.vo.auto_send_log;
 
-public interface AutoService {
-	int auto_total(auto_send paramauto_send);
+@Service
+public class AutoService {
 
-	List<auto_send> auto_check(auto_send paramauto_send);
+	@Autowired
+	private AutoMapper autoMapper;
 
-	void auto_update(auto_send paramauto_send);
+	public void auto_update(auto_send auto) {
+		this.autoMapper.auto_update(auto);
+	}
 
-	void auto_log_insert(auto_send_log paramauto_send_log);
+	public List<auto_send> auto_check(auto_send auto) {
+		return this.autoMapper.auto_check(auto);
+	}
+
+	public int auto_total(auto_send auto) {
+		return this.autoMapper.auto_total(auto);
+	}
+
+	public void auto_log_insert(auto_send_log logs) {
+		this.autoMapper.auto_log_insert(logs);
+	}
 }
